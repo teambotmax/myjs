277e5de6705f2daac76a9cbc866610fb24078cbe
Yepz <41275566+Myyepz@users.noreply.github.com>
2019-09-14 00:15:10 +0700

    Add files via upload

---
+++ src/bot.js
@@ -0,0 +1,41 @@
+const LineConnect = require('./connect');
+const LINE = require('./main.js');
+const fs = require('fs');
+console.info("\n\
+Thanks to @Alfathdirk @TCR_TEAM\n\
+\nNB : Remake From @Myyepz \n\
+***Copyright belongs to the author***");
+
+/*
+| This constant is for auth/login
+| 
+| Change it to your authToken / your email & password
+*/
+//TRIO_PEKOK_PROTECTION
+
+const auth = {
+    authToken: '',
+	certificate: '',
+	email: 'wongkito07.lp@gmail.com',
+	password: '123syarif12'
+}
+
+let client =  new LineConnect();
+//let client =  new LineConnect(auth);
+
+client.startx().then(async (res) => {
+	while(true) {
+		try {
+			ops = await client.fetchOps(res.operation.revision);
+		} catch(error) {
+			console.log('error',error)
+		}
+		for (let op in ops) {
+			if(ops[op].revision.toString() != -1){
+				res.operation.revision = ops[op].revision;
+				LINE.poll(ops[op])
+			}
+		}
+		//LINE.aLike() //AutoLike (CAUSE LAG)
+	}
+});
